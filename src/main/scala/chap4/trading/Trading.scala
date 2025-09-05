cea66653fb5c64ff1f794e30f27616192a63b6f4
sanjaythangaraj <sanjaythangaraj@outlook.in>
2025-09-05 20:00:52 +0530

    chap4 - trading domain model - Kleisli composition

---
+++ src/main/scala/chap4/trading/Trading.scala
@@ -0,0 +1,23 @@
+package chap4.trading
+
+import scalaz._
+import Scalaz._
+
+trait Trading[Account, Trade, ClientOrder, Order, Execution, Market] {
+  def clientOrders: Kleisli[List, List[ClientOrder], Order]
+  def execute(
+      market: Market,
+      brokerAccount: Account
+  ): Kleisli[List, Order, Execution]
+  def allocate(accounts: List[Account]): Kleisli[List, Execution, Trade]
+
+  def tradeGeneration(
+      market: Market,
+      broker: Account,
+      clientAccounts: List[Account]
+  ): Kleisli[List, List[ClientOrder], Trade] = {
+    clientOrders andThen
+      execute(market, broker) andThen
+      allocate(clientAccounts)
+  }
+}
